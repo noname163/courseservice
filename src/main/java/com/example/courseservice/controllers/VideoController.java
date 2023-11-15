9228ca6401a62bb72f17fccff65c5fc23a7b3f70
danghdat <danghdat@hanatourjsv.com>
2023-11-15 12:20:53 +0700

    feat(CEPA-201): change method to put for video update

--- src/main/java/com/example/courseservice/controllers/VideoController.java
+++ src/main/java/com/example/courseservice/controllers/VideoController.java
@@ -12,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -75,7 +76,7 @@ public class VideoController {
             })
     })
     @PreAuthorize("hasAnyAuthority('STUDENT', 'TEACHER')")
-    @PostMapping("/update")
+    @PutMapping("/update")
     public ResponseEntity<Void> updateVideo(
             @RequestPart VideoUpdateRequest videoRequest,
             @RequestPart() MultipartFile video,
