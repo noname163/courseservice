4516bd2291ecc66459cd1a30622313cf69525227
danghdat <danghdat@hanatourjsv.com>
2023-11-15 16:51:52 +0700

    feat(CEPA-201): update video order

--- src/main/java/com/example/courseservice/data/dto/request/CourseUpdateRequest.java
+++ src/main/java/com/example/courseservice/data/dto/request/CourseUpdateRequest.java
@@ -1,5 +1,7 @@
 package com.example.courseservice.data.dto.request;
 
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,4 +13,5 @@ public class CourseUpdateRequest {
     private String name;
     private Double price;
     private Long levelId;
+    private List<VideoOrder> videoOrders;
 }
--- src/main/java/com/example/courseservice/data/repositories/VideoRepository.java
+++ src/main/java/com/example/courseservice/data/repositories/VideoRepository.java
@@ -2,10 +2,13 @@ package com.example.courseservice.data.repositories;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.courseservice.data.constants.CommonStatus;
 import com.example.courseservice.data.entities.Course;
@@ -18,6 +21,9 @@ public interface VideoRepository extends JpaRepository<Video, Long> {
     Page<Video> findByCourseIn(List<Course> courses,Pageable pageable);
     List<Video> findByCourseAndStatus(Course course, CommonStatus status);
     List<Video> findByCourseAndStatusNot(Course course, CommonStatus status);
+    List<Video> findByCourseIdAndIdIn(Long courseId, Set<Long> ids);
     Optional<Video> findByIdAndStatus(Long id, CommonStatus status);
     Optional<Video> findByIdAndStatusNot(Long id, CommonStatus status);
+    @Query("SELECT MAX(v.ordinalNumber) FROM Video v WHERE v.course = :course")
+    Integer findMaxOrdinalNumberByCourse(@Param("course") Course course);
 }
