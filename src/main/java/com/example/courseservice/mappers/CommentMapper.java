e2666ffc054d4d3ccc0eecb2554e376a0bda6735
danghdat <danghdat@hanatourjsv.com>
2023-12-12 15:04:39 +0700

    feat(CEPA-308): modify video detail response

--- src/main/java/com/example/courseservice/mappers/CommentMapper.java
+++ src/main/java/com/example/courseservice/mappers/CommentMapper.java
@@ -1,6 +1,8 @@
 package com.example.courseservice.mappers;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
@@ -24,13 +26,16 @@ public class CommentMapper {
                 .email(comment.getStudentEmail())
                 .avatar(comment.getUserAvatar())
                 .useName(comment.getUserName())
-                .createdDate(comment.getCreatedDate().toLocalDate())
+                .createdDate(Optional.ofNullable(comment.getCreatedDate().toLocalDate()).orElse(null))
                 .upDateTime(comment.getUpdateTime())
                 .comment(comment.getCommented())
                 .build();
     }
 
     public List<CommentResponse> mapEntitiesToDtos(List<Comment> comments) {
+        if(comments.isEmpty()){
+            return List.of();
+        }
         return comments.stream().map(this::mapEntityToDto).collect(Collectors.toList());
     }
 }
